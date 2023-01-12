3fc1ca566e1b7b7d47b766eb99e0dde63e568b25
Paulo Tinoco <paulovictortinoco@gmail.com>
2023-01-12 16:12:23 -0300

    fix: Adjusting index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from './message-translators';
+export { AbstractMessageTranslator, StringMessageTranslator } from './message-translators';
 export { default as config } from './config';
 export { default as logging } from './logging';
-export { default as AbstractMessageTranslator } from './providers';
+export { default as AbstractProvider } from './providers';
 export { default as EvoluttyManager } from './managers';
 export { default as Router } from './routes';
 export { default as BullMQRouter } from './ext/bullmq/routes';
