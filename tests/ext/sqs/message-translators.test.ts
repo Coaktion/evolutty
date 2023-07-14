acf4146048c7c9d001e2b1741420dc31ccebb8f4
gustavo <gus.inacio@hotmail.com>
2023-07-14 10:16:27 -0300

    test: add sqs message translator tests

--- tests/ext/sqs/message-translators.test.ts
+++ tests/ext/sqs/message-translators.test.ts
@@ -1,39 +1,77 @@
-import { SQSMessageTranslator } from '../../../src';
+import { SNSQueueMessageTranslator, SQSMessageTranslator } from '../../../src';
 
-describe('SQSMessageTranslator', () => {
-  const translator = new SQSMessageTranslator();
-
-  it('should translate message correctly', () => {
-    const message = {
-      Body: JSON.stringify({
-        MessageId: '123',
-        Message: JSON.stringify({ foo: 'bar' }),
-        ReceiptHandle: '321',
-        MD5OfBody: 'abc',
-        Attributes: {},
-        MessageAttributes: {},
-        MD5OfMessageAttributes: '',
-        EventSource: '',
-        EventSourceARN: '',
-        AwsRegion: ''
-      })
-    };
-
-    const expected = {
-      content: { foo: 'bar' },
-      metadata: {
-        MessageId: '123',
-        ReceiptHandle: '321',
-        MD5OfBody: 'abc',
-        Attributes: {},
-        MessageAttributes: {},
-        MD5OfMessageAttributes: '',
-        EventSource: '',
-        EventSourceARN: '',
-        AwsRegion: ''
-      }
-    };
-
-    expect(translator.translateMessage(message)).toEqual(expected);
+describe('Message translators', () => {
+  describe('SNSQueueMessageTranslator', () => {
+    const translator = new SNSQueueMessageTranslator();
+  
+    it('should translate message correctly', () => {
+      const message = {
+        Body: JSON.stringify({
+          MessageId: '123',
+          Message: JSON.stringify({ foo: 'bar' }),
+          ReceiptHandle: '321',
+          MD5OfBody: 'abc',
+          Attributes: {},
+          MessageAttributes: {},
+          MD5OfMessageAttributes: '',
+          EventSource: '',
+          EventSourceARN: '',
+          AwsRegion: ''
+        })
+      };
+  
+      const expected = {
+        content: { foo: 'bar' },
+        metadata: {
+          MessageId: '123',
+          ReceiptHandle: '321',
+          MD5OfBody: 'abc',
+          Attributes: {},
+          MessageAttributes: {},
+          MD5OfMessageAttributes: '',
+          EventSource: '',
+          EventSourceARN: '',
+          AwsRegion: ''
+        }
+      };
+  
+      expect(translator.translateMessage(message)).toEqual(expected);
+    });
   });
-});
+
+  describe('SQSMessageTranslator', () => {
+    const translator = new SQSMessageTranslator();
+  
+    it('should translate message correctly', () => {
+      const message = {
+        Body: JSON.stringify({ foo: 'bar' }),
+        MessageId: '123',
+        ReceiptHandle: '321',
+        MD5OfBody: 'abc',
+        Attributes: {},
+        MessageAttributes: {},
+        MD5OfMessageAttributes: '',
+        EventSource: '',
+        EventSourceARN: '',
+        AwsRegion: ''
+      };
+  
+      const expected = {
+        content: { foo: 'bar' },
+        metadata: {
+          MessageId: '123',
+          ReceiptHandle: '321',
+          MD5OfBody: 'abc',
+          Attributes: {},
+          MessageAttributes: {},
+          MD5OfMessageAttributes: '',
+          EventSource: '',
+          EventSourceARN: '',
+          AwsRegion: ''
+        }
+      };
+  
+      expect(translator.translateMessage(message)).toEqual(expected);
+    });
+  });
+})
