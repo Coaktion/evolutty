f6d3676311d1ae9a923acad8e535421aa6182ef6
gustavo <gus.inacio@hotmail.com>
2023-07-14 10:20:17 -0300

    feat: add sqs queue router

--- src/ext/sqs/routes.ts
+++ src/ext/sqs/routes.ts
@@ -1,3 +1,5 @@
+import { SNSQueueMessageTranslator, SQSMessageTranslator } from "./message-translators";
+
 export class SQSRouter {
   queueName: string;
   handler: any;
@@ -12,7 +14,30 @@ export class SQSRouter {
     }
 
     this.queueName = queueName;
+    clientOptions.messageTranslator = new SQSMessageTranslator();
     this.handler = new handler(this.queueName, clientOptions);
     this.handler.start();
   }
 }
+
+export class SNSQueueRouter {
+  queueName: string;
+  handler: any;
+  constructor(
+    queueName: string,
+    handler: any,
+    clientOptions: any,
+    ..._args: any[]
+  ) {
+    if (!queueName) {
+      throw new Error('Queue name must be provided');
+    }
+
+    this.queueName = queueName;
+    clientOptions.messageTranslator = new SNSQueueMessageTranslator();
+    this.handler = new handler(this.queueName, clientOptions);
+    this.handler.start();
+  }
+}
+
+
