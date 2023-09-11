4cb90b73ceca6f1f9dc1521b7c2ed851db19dc1b
gustavo <gus.inacio@hotmail.com>
2023-09-11 16:26:19 -0300

    fix: set default transports array

--- src/logging.ts
+++ src/logging.ts
@@ -2,18 +2,23 @@ import { createLogger, format, transports } from 'winston';
 
 const level = process.env.LOG_LEVEL || 'info';
 
-const logTransports = process.env.LOG_TRANSPORTS.split(',');
+const logTransports = process.env.LOG_TRANSPORTS
+  ? process.env.LOG_TRANSPORTS.split(',')
+  : ['console'];
 
 const logging = createLogger({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.errors({ stack: true }),
-    process.env.LOG_FORMAT ? format[process.env.LOG_FORMAT]() : format.json(),
+    process.env.LOG_FORMAT ? format[process.env.LOG_FORMAT]() : format.json()
   ),
   transports: logTransports.map((transport: string) => {
     switch (transport.trim()) {
       case 'file':
-        return new transports.File({ filename: process.env.LOG_OUTPUT_FILE, level });
+        return new transports.File({
+          filename: process.env.LOG_OUTPUT_FILE,
+          level
+        });
       case 'console':
         return new transports.Console({ level });
       default:
