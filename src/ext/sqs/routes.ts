533b51917a13bc7a464ff0325037e7ce2f4ba9a7
Gustavo Inacio <gus.inacio@hotmail.com>
2023-10-18 13:51:53 -0300

    feat(sqs): consumes multiples queues per route

--- src/ext/sqs/routes.ts
+++ src/ext/sqs/routes.ts
@@ -1,18 +1,23 @@
+import { ListQueuesCommand } from '@aws-sdk/client-sqs';
+import { BaseClient } from './base';
 import {
   SNSQueueMessageTranslator,
   SQSMessageTranslator
 } from './message-translators';
 import { SQSClientOptions } from './types';
 
-export class SQSRouter {
+export class SQSRouter extends BaseClient {
   queueName: string;
   handler: any;
+  clientOptions: SQSClientOptions;
   constructor(
     queueName: string,
     handler: any,
     clientOptions: SQSClientOptions,
     ..._args: any[]
   ) {
+    super(clientOptions)
+
     if (!queueName) {
       throw new Error('Queue name must be provided');
     }
@@ -24,7 +29,29 @@ export class SQSRouter {
     }
 
     this.queueName = queueName;
+    this.clientOptions = clientOptions;
+
+    if (clientOptions.prefixBasedQueues) {
+      this.handlePrefixBasedQueues(queueName, handler);
+      return;
+    }
+
     this.handler = new handler(this.queueName, clientOptions);
     this.handler.start();
+  }
+
+  private async handlePrefixBasedQueues(prefix: string, handler: any) {
+    const data = await this.client.send(
+      new ListQueuesCommand({
+        QueueNamePrefix: prefix
+      })
+    )
+
+    const urls = data.QueueUrls || [];
+
+    for (const queueUrl of urls) {
+      const queueHandler = new handler(queueUrl, this.clientOptions);
+      queueHandler.start();
+    }
   }
 }
